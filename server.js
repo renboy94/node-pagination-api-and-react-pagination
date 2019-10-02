4beef0e4600a05fbf093b18fdc338e9e1e38f3d9
Rendell Lasola <rendelllasola@gmail.com>
2019-10-02 10:39:22 +0800

    pagination with mongodb data

--- server.js
+++ server.js
@@ -1,36 +1,45 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const User = require("./users");
 
-// app.use(express.json());
+mongoose.connect(
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds229108.mlab.com:29108/pagination`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+const db = mongoose.connection;
+db.once("open", async () => {
+  if ((await User.countDocuments().exec()) > 0) return;
 
-const users = [
-  { id: 1, name: "User 1" },
-  { id: 2, name: "User 2" },
-  { id: 3, name: "User 3" },
-  { id: 4, name: "User 4" },
-  { id: 5, name: "User 5" },
-  { id: 6, name: "User 6" },
-  { id: 7, name: "User 7" },
-  { id: 8, name: "User 8" },
-  { id: 9, name: "User 9" },
-  { id: 10, name: "User 10" },
-  { id: 11, name: "User 11" },
-  { id: 12, name: "User 12" },
-  { id: 13, name: "User 13" },
-  { id: 14, name: "User 14" },
-  { id: 15, name: "User 15" }
-];
+  Promise.all([
+    User.create({ name: "User 1" }),
+    User.create({ name: "User 2" }),
+    User.create({ name: "User 3" }),
+    User.create({ name: "User 4" }),
+    User.create({ name: "User 5" }),
+    User.create({ name: "User 6" }),
+    User.create({ name: "User 7" }),
+    User.create({ name: "User 8" }),
+    User.create({ name: "User 9" }),
+    User.create({ name: "User 10" }),
+    User.create({ name: "User 11" }),
+    User.create({ name: "User 12" }),
+    User.create({ name: "User 13" }),
+    User.create({ name: "User 14" }),
+    User.create({ name: "User 15" })
+  ]).then(() => console.log("Added Users"));
+});
 
-app.get("/users", paginatedResults(users), (req, res) => {
+app.get("/users", paginatedResults(User), (req, res) => {
   res.json(res.paginatedResults);
 });
 
-app.get("/hey", (req, res) => {
-  res.send("hey");
-});
-
 function paginatedResults(model) {
-  return (req, res, next) => {
+  return async (req, res, next) => {
     const page = parseInt(req.query.page);
     const limit = parseInt(req.query.limit);
 
@@ -39,7 +48,7 @@ function paginatedResults(model) {
 
     const results = {};
 
-    if (endIndex < model.length) {
+    if (endIndex < (await model.countDocuments().exec())) {
       results.next = {
         page: page + 1,
         limit: limit
@@ -53,10 +62,17 @@ function paginatedResults(model) {
       };
     }
 
-    results.results = model.slice(startIndex, endIndex);
-
-    res.paginatedResults = results;
-    next();
+    try {
+      results.results = await model
+        .find()
+        .limit(limit)
+        .skip(startIndex)
+        .exec();
+      res.paginatedResults = results;
+      next();
+    } catch (e) {
+      res.status(500).json({ message: e.message });
+    }
   };
 }
 
